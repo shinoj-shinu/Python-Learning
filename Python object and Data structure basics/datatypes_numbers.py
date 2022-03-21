c98ffa4fcb895b9acac246eaaaf1cc36249efcb5
anupama cv <anu.anupamacv@gmail.com>
2022-03-21 10:32:21 +0530

    Added: example in number datatype

---
+++ Python object and Data structure basics/datatypes_numbers.py
@@ -0,0 +1,36 @@
+"""
+Types of numbers in python
+numbers is a basic data types in python and types of numbers are Integers and Float
+Integers
+integers are just whole numbers,positive or negative
+
+float
+Floating numbers in python are notable because they have decimal point in them.For ex- 2.0, -2.1
+"""
+# basic arithmetic
+
+print(1 + 2)
+# subtraction
+print(5 - 2)
+# floor_division
+print(6 // 4)
+'''
+Floor division
+using "floor" division. The // operator (two forward slashes) truncates the decimal without rounding, 
+and returns an integer result.'''
+
+# modulo
+print(7 % 4)
+'''4 goes into 7 once, with a remainder of 3. The % operator returns the remainder after division'''
+
+# powers
+print(2 ** 3)
+
+# can also do root this way
+print(4**0.5)
+
+# Order of Operations followed in Python
+print(2 + 3 - 1 * 3)
+
+# Can use parentheses to specify orders
+print((2+10) * (10+3))
