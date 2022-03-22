63e8d6a4b0c71fde8f1ca88ec4cadc0d8b2afd0e
anupama cv <anu.anupamacv@gmail.com>
2022-03-22 11:23:45 +0530

    Added: examples

---
+++ Python object and Data structure basics/Variable_assignment.py
@@ -0,0 +1,39 @@
+""" Variable is a containers for storing the data values
+Rules
+names cannot start with the numbers
+there can be no spaces in the name,use _ instead
+can't use special characters : <> ? @ % +
+it is considered best practice that the names are lowerase.
+avoid using the words that have special meaning in python list 'list and 'str' etc
+python uses dynamic typing (this means can reassign the variables to different data types
+
+my_dogs = 2
+my_dogs = ["sammy" , "frankee"]
+this is totally okay in python
+
+for find out the datatype use type() function
+
+"""
+a = 'shinoj'
+print(a)
+b = 3
+print(b)
+
+c = 5
+print(c)
+print(type(c))
+
+print(type(a))
+
+f = 1 + 7 - 6
+print(f)
+print(type(f))
+
+a = 6.5
+print(type(a))
+
+my_income = 100
+tax_rate = 2.5
+
+print(my_income * tax_rate)
+
