aabac9d2c9903d74c47dc2df607199ada69112bc
anupama cv <anu.anupamacv@gmail.com>
2022-03-22 14:55:04 +0530

    adding examples of datatypes string

---
+++ Python object and Data structure basics/Indexing_slicing.py
@@ -0,0 +1,28 @@
+'''indexing allows you to to grab a single character from the string
+indexing notation uses[] notation after the string(or variable assigned the string)
+
+character    : h  e  i  i  o
+index        : o  1  2  3  4
+reverse index: 0 -4 -3 -3 -2
+
+slicing allows to grab a subsection of multiple characters,a slice of the string
+
+[start:stop:step]
+
+start is a numerical index for the slice start
+stop is the index you will go up to (but not include)
+step is the size of the "jump" you take
+'''
+
+print("shinoj tharol")
+print("shinoj \t world")
+print("shinoj \n world")
+print("shinoj \nworld")
+
+##indexing and slicing
+my_string = "hello world"
+print(my_string)
+print(len(my_string))
+
+print(my_string[0])
+print(my_string[4])
---
+++ Python object and Data structure basics/datatypes_strings.py
@@ -0,0 +1,21 @@
+"""
+Strings are sequences of characters,using the syntax of either single quotes or double quotes
+'hello'
+"hello"
+"I want to learn"
+"""
+
+a = "hello world"
+print(a)
+print("shinoj")
+a = 'hello'
+print(a)
+print("shinoj tharol")
+print('shinoj \n world')
+print('shinoj \nworld')
+print('shinoj \t world')
+
+#Length function
+
+a = 'hello world'
+print(len(a))
