34d4b93e37833862de1fb6427d1424218d05e6eb
anupama cv <anu.anupamacv@gmail.com>
2022-03-27 12:35:36 +0530

    examples of tuples

---
+++ Python object and Data structure basics/datatypes_tuples.py
@@ -0,0 +1,14 @@
+"""tuples are similar to list.However they have one key difference -immutability (cannot change)
+once an element is inside a tuple ,it cannot be reassigned
+tuples use (1,2,3)"""
+
+t = (1,3,4,5)
+print(t)
+print(type(t))
+print(len(t))
+print(t[0])
+
+print(t.count(1))
+print(t.index(3))
+print(t.index(4))
+
