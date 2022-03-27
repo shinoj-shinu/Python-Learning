dc79ffaa6dc31ee193137be34bd65a0c2aca0fc5
anupama cv <anu.anupamacv@gmail.com>
2022-03-27 13:05:17 +0530

    datatypes_sets

---
+++ Python object and Data structure basics/datatypes_sets.py
@@ -0,0 +1,14 @@
+"""sets are unordered collections of unique elements ,there only be one representative of the same object"
+"""
+
+myset = set()
+print(myset)
+myset.add(1)
+print(myset)
+myset.add(2)
+print(myset)
+
+mylist = [ 1,1,1,2,2,2,2]
+print(mylist)
+print(set(mylist))
+
