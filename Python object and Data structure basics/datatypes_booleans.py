5caf2291e0bc2ff0d3e41912c7de8092652d006f
anupama cv <anu.anupamacv@gmail.com>
2022-03-28 11:40:23 +0530

    datatypes_boolean

---
+++ Python object and Data structure basics/datatypes_booleans.py
@@ -0,0 +1,15 @@
+"""booleans are operators that allow you to convey True or False statements
+"""
+
+a = 1 > 2
+print(a)
+print(type(a))
+
+# none
+
+a = None
+
+print(a)
+print(type(a))
+
+myfile = open()
