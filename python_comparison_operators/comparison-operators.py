cd08062d4ae58ba2cc9acf980ba8d4f6d983bd8d
anupama cv <anu.anupamacv@gmail.com>
2022-03-28 11:54:39 +0530

    comparison operators

---
+++ python_comparison_operators/comparison-operators.py
@@ -0,0 +1,40 @@
+"""
+comparison operators will allow us to compare variables and output is a boolean value
+
+==  # if the values of two variables are equal,then condition becomes true    (a==b) is not true
+
+!=  # if values of two variables are not equal,then condition becomes true    (a!=b) is true
+
+>   # if the value of left operand is greater than the value of right operand,then conditions becomes true (a>b) is not true
+
+<   # if the value of left operand is less than the value of right operand,then conditions becomes true (a<b) is true
+
+>=  # if the value of left operand is greater or equal to the value of right operand,then conditions becomes true (a>=b) is not true
+
+<=  # if the value of left operand is less than the value of right operand,then conditions becomes true (a<=b) is true
+
+
+"""
+
+print(2 == 1)
+
+print("hello" =="bye")
+
+print("Bye" == "bye")
+
+print("2" == 2)
+
+print(2.0 == 2)
+
+print(3 !=3)
+
+print(4 !=5)
+
+print(2 >1)
+
+print(1 >2)
+
+print(1 < 3)
+
+print(2 < 1)
+
