18c9954d3e3c2e0a3d03406f1f4234ab789bb7e9
anupama cv <anu.anupamacv@gmail.com>
2022-03-29 11:01:51 +0530

    logical_operators

---
+++ python_comparison_operators/logical_operators.py
@@ -0,0 +1,22 @@
+"""logical operators use to combine comparisons
+and , or ,not are logical operators
+"""
+print(1 < 2)
+print(2 < 3)
+
+print(1 < 2 < 3)
+
+print(1 < 2 > 3)
+
+print(1 < 2 and 2 < 3)
+
+print("h" == "h" and 2 == 2)
+
+print(1 == 1 or 2 ==2)
+
+print(1 == 1)
+
+print(not 1 == 1)
+
+print(not 400 > 5000)
+    
