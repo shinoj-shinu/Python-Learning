dc283a542a8b9d5861d8f3b03cef98c3ffaa643c
anupama cv <anu.anupamacv@gmail.com>
2022-03-30 11:36:53 +0530

    concept of if,elif,else statements

---
+++ python_statements/if_elif_else_statements.py
@@ -0,0 +1,64 @@
+"""python statements are used for decision-making
+we often only want certain code to execute when a particular condition has been met
+
+note - control flow syntax makes use of colons and indentation(whitespace)
+
+if statement # some_condition execute
+elif statement # some_other_condition
+else statement # do something different
+"""
+
+if True:
+    print("its true")
+
+if 3>2:
+    print("its true")
+
+hungry = False
+
+hungry = True
+
+if hungry:
+    print("feed me ")
+
+if hungry:
+    print("feed me ")
+else:
+    print("iam not hungry")
+
+loc = 'Auto shop'
+
+if loc == "Auto shop":
+    print("cars are cool")
+else:
+    print("i do not know much")
+
+if loc == "Auto shop":
+    print("cars are cool")
+elif loc == "Bank":
+    print("money is cool")
+else:
+    print("i do not know much")
+
+loc = 'Shop'
+
+if loc == "Auto shop":
+    print("cars are cool")
+elif loc == "Bank":
+    print("money is cool")
+elif loc == "store":
+    print("welcome to the store")
+else:
+    print("i do not know much")
+
+
+loc = 'game'
+
+if loc == "Auto shop":
+    print("cars are cool")
+elif loc == "Bank":
+    print("money is cool")
+elif loc == "store":
+    print("welcome to the store")
+else:
+    print("i do not know much")
