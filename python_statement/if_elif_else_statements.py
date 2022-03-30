95a3a05e24065a81d903e15b05124e5b9f00a603
anupama cv <anu.anupamacv@gmail.com>
2022-03-30 10:37:25 +0530

    if_elif_else_statements

---
+++ python_statement/if_elif_else_statements.py
@@ -0,0 +1,12 @@
+"""we often only want certain code to execute when a particular condition has been met
+if, elif, else are the keywords
+
+note- control flow syntax makes use of colons and indentation(whitespaces)
+"""
+
+# if statement
+
+#  if some_condition:
+# execute some code
+
+
